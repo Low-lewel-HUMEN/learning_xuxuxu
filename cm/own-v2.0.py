71ed8631b6fa7ab69e0f454b352eac36e571d000
Роман <malukin2011@gmail.com>
2026-07-04 10:34:19 +0300

    cm-from-own-v2.0

---
+++ cm/own-v2.0.py
@@ -0,0 +1,13 @@
+v-2.0 
+code code codelkjkweqd
+\e
+fe
+fe
+fewgwrgwrpfouehifjqhu3fuhij4fb4 f4ff14f 42uf 4g4fg2f4 
+g4 g4 g 4g ff 4f4 g 34f 4 g43 f 4f 4g 4 f 43g 34g43f 43 g
+elifqwevebcuij3rwubihjocr3uvij4dpi3ruijd34icruij4di3rvuip3dj4ifrv
+2q3rhij30irofj4-0ifj3rh4j9 43g59jf45ihtbj053kfjgi4tbjg5 435g0t4j940i3g5hbt
+3rtw4gj930i395jghj3f94gthboijf035g94tj530fg-9t  35gtb4j9-035igj9-95jti
+tbjhj4f9-35gj9thboi4j53fi0gj450453gj03gtboj5g0i3jo0k4p0ktbi
+43gtbjg5403gji-4035gj4o03gj0tigj3o40i3gteoi3p420gteojg305etbjg5
+4f]3pgktobijpo4k3gjiten42[fejkr93gpoj3frpigj349-giphjgjij-]
